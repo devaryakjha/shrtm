c21e83356224d7f9d245d2b02c0334f6b47b5b0a
devaryakjha <dev.jha.arya@gmail.com>
2023-10-26 00:48:10 +0530

    ✨ New: completed Initial UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,55 @@
+import type { DetailedHTMLProps, InputHTMLAttributes } from "react";
+import styles from "./page.module.css";
+
 export default function Home() {
-  return <main className="" />;
+  return (
+    <main className={styles.main}>
+      <div className={styles.formcard}>
+        <div className="p-2">
+          <form className={styles.form}>
+            <div className={styles.formSection}>
+              <Input
+                id="input-url"
+                type="text"
+                aria-label="Long Url"
+                placeholder="Enter long link here"
+                label="Shorten your URL"
+              />
+              <Input
+                id="alias"
+                type="text"
+                aria-label="Custom Alias"
+                placeholder="Enter your alias here"
+                label="Customize your Link"
+              />
+            </div>
+            <button className={styles.submit} type="submit">
+              Shorten URL
+            </button>
+          </form>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+interface InputProps
+  extends DetailedHTMLProps<
+    InputHTMLAttributes<HTMLInputElement>,
+    HTMLInputElement
+  > {
+  label?: string;
+}
+
+function Input(props: InputProps) {
+  return (
+    <div>
+      {props.label && (
+        <label htmlFor={props.id}>
+          <span className={styles.label}>{props.label}</span>
+        </label>
+      )}
+      <input className={styles.input} {...props} />
+    </div>
+  );
 }
