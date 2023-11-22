18437f66ab4ddecf1da5ff01e5010fcd080dee7f
devaryakjha <dev.jha.arya@gmail.com>
2023-11-22 23:07:57 +0530

    Fix configuration and update dependencies

--- src/app/components/submit_form.tsx
+++ src/app/components/submit_form.tsx
@@ -1,8 +1,6 @@
 "use client";
 
-// ts-ignore because experimental_useFormStatus is not in the types
-// @ts-ignore
-import { experimental_useFormStatus as useFormStatus } from "react-dom";
+import { useFormStatus } from "react-dom";
 import type React from "react";
 
 interface SubmitProps
@@ -12,6 +10,6 @@ interface SubmitProps
   > {}
 
 export default function Submit(props: SubmitProps) {
-  const { pending }: import("react-dom").FormStatus = useFormStatus();
+  const { pending } = useFormStatus();
   return <button type="submit" {...props} disabled={pending} />;
 }
